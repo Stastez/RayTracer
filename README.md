60eb197d7d1af16ad5d4475fc143aa74d6af1d54
stastez <78495486+Stastez@users.noreply.github.com>
2024-01-06 19:20:27 +0100

    Make config passable, create license and readme

---
+++ README.md
@@ -0,0 +1,17 @@
+# RayTracer
+## Why?
+This is a toy project to...
+- further brush up my skills in computer graphics and
+- try to learn C++.
+
+As such, this project is not intended to fulfill an actual purpose outside the aforementioned ones and may (nay, will)
+contain bugs, inaccuracies, bad practices and general nastiness. If you don't mind these limitations, have fun!
+
+## How?
+This project requires...
+- [glm](https://github.com/g-truc/glm)
+- [libPNG](http://www.libpng.org/pub/png/libpng.html)
+
+Both are available from [vcpkg](https://vcpkg.io/en/).
+
+Provide a config file in the same format as [config.cfg](config.cfg) as a command line parameter to specify the scene to use.
--- main.cpp
+++ main.cpp
@@ -6,13 +6,17 @@
 #include "PngWriter.h"
 #include "Scene.h"
 
-int main()
+int main(int argc, char* argv[])
 {
-    auto [outputPath, resolutionX, resolutionY, objects] = RayTracer::ConfigReader::readConfig("/media/stastez/SN850X/code/raytracer/config.cfg");
+    if (argc < 2)
+    {
+        std::cout << "Usage: RayTracer [config]" << std::endl;
+        exit(1);
+    }
+
+    auto [outputPath, resolutionX, resolutionY, objects] = RayTracer::ConfigReader::readConfig(argv[1]);
 
     const auto scene = RayTracer::Scene(std::move(objects));
-
-    auto ray = RayTracer::Ray();
 
     auto pic = RayTracer::Picture(resolutionX, resolutionY);
 
@@ -22,7 +26,7 @@ int main()
     {
         for (unsigned y = 0; y < resolutionY; ++y)
         {
-            ray = RayTracer::Ray(glm::vec3(static_cast<float>(x) / static_cast<float>(resolutionX) * aspectRatio, static_cast<float>(y) / static_cast<float>(resolutionY), 0), glm::vec3(0, 0, 1));
+            const auto ray = RayTracer::Ray(glm::vec3(static_cast<float>(x) / static_cast<float>(resolutionX) * aspectRatio, static_cast<float>(y) / static_cast<float>(resolutionY), 0), glm::vec3(0, 0, 1));
             const auto [exists, position, distance, color] = scene.intersect(ray);
             if (exists) pic.setPixel(x, y, (1 - distance) * color);
             else pic.setPixel(x, y, std::array<png_byte, 3>{0, 0, 0});
